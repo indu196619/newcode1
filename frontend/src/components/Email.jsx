d9842dc95fcb0363637e7c794c699ae6966e6c50
prashithak <135314888+prashithak@users.noreply.github.com>
2024-05-15 14:34:51 +0530

    email edit

--- frontend/src/components/Email.jsx
+++ frontend/src/components/Email.jsx
@@ -4,6 +4,9 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import SendIcon from '@mui/icons-material/Send';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Box, Grid, Typography } from '@mui/material';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 let img={
     backgroundImage:`url('https://mrwallpaper.com/images/hd/animated-design-chromebook-background-e3s56alv0sopjtap.jpg')`,
@@ -18,27 +21,33 @@ let img={
 }
 
 const Email = () => {
+  const navigate = useNavigate();
 
-  const[email,setEmail]=useState("");
-  const[sub,setSub]=useState("");
-  const doc1="https://docs.google.com/document/d/15nq6PJmcIY3jgwPl3BkCMMgmal-pJZGgDGqY46tE9Ew/edit?usp=sharing"
-const sendEmail= async(e)=>{
-  e.preventDefault();
-  const res = await fetch("/mail",{
-    method:"POST",
-    headers:{
-      "content-type":"application/json"
-    },
-    body:JSON.stringify({email})
-  
-    
-  });
-  console.log(res);
-  alert("successfully mailed");
+  const[email,setEmail]=useState({recipient:'',subject:'',doc:''});
+  // const[sub,setSub]=useState("");
+  // const[doc,setDoc]=useState("");
+  // const doc1="https://docs.google.com/document/d/15nq6PJmcIY3jgwPl3BkCMMgmal-pJZGgDGqY46tE9Ew/edit?usp=sharing"
+
+const handleChange=(e)=>{
+  const{name,value}=e.target;
+  setEmail({...email,[name]:value});
 }
 
+
+const sendEmail= async(e)=>{
+  e.preventDefault();
+  console.log(email.recipient);
+  console.log(email.subject);
+  console.log(email.doc);
+  axios.post('http://localhost:3001/mail',email)
+  .then(respose=>{alert("success");
+    navigate('/addash');
+  })
+  .catch(error=>{console.log(error);});
+};
+
   return (
-    <div style={img}>
+    <div >
     <div><E_nav/></div>
         <div className="container ">
             <div className='d-flex justify-content-center mt-4'> 
@@ -49,33 +58,68 @@ const sendEmail= async(e)=>{
             <Form className='mt-4 col-lg-4'>
       <Form.Group  controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email"  name='email' onChange={(e)=>{setEmail(e.target.value)}} />
+        <Form.Control type="email"  name='recipient' value={email.recipient} onChange={handleChange} />
       </Form.Group>
 
       <Form.Group  controlId="formBasicEmail">
         <Form.Label>Subject</Form.Label>
-        <Form.Control type="text"  name='sub' onChange={(e)=>{setSub(e.target.value)}}/>
+        <Form.Control type="text"  name='subject' value={email.subject} onChange={handleChange}/>
       </Form.Group>
 
-      <Button
-  component="label"
-  role={undefined}
-  variant="contained"
-  tabIndex={-1}
-  startIcon={<CloudUploadIcon />}
-  style={{marginLeft:'35%',marginTop:'5%'}}
->
-  Upload mark sheet
-  <input type="file" />
-</Button>
+      <Form.Group  controlId="formBasicEmail">
+        <Form.Label>Documenet link</Form.Label>
+        <Form.Control type="text"  name='doc' value={email.doc} onChange={handleChange}/>
+      </Form.Group>
+     
        <br /><br /><br />
-       <Button variant="primary" type="submit" style={{marginLeft:'30%'}} onClick={sendEmail} ><SendIcon/>send Mail
-        
-      </Button>
+       <Button variant="primary" type="submit" style={{marginLeft:'30%'}} onClick={sendEmail} ><SendIcon/>send Mail</Button>
     </Form>
             </div>
         </div>
-    </div>
+    <Grid item xs={12}>
+    <Box sx={{marginTop:'5em', bgcolor: 'grey', color: 'white', textAlign: 'center', py: 2 }}>
+      {/* Footer content */}
+      <Typography variant="h6" gutterBottom>
+        Contact Information
+      </Typography>
+      <Grid container justifyContent="center" spacing={4}>
+        {/* Headquarters */}
+        <Grid item>
+          <Typography variant="body1">
+            <strong>Headquarters:</strong><br />
+            <h5>G1, Ground Floor, Thejaswini, Technopark Campus<br />
+            Thiruvananthapuram, Kerala, India - 695 581</h5>
+          </Typography>
+        </Grid>
+
+        {/* Regional Centre (North) */}
+        <Grid item>
+          <Typography variant="body1">
+            <strong>Regional Centre (North):</strong><br />
+           <h5> 2nd Floor, UL Cyberpark Building, Nellikode Post<br />
+            Kozhikode, Kerala, India - 673 016</h5>
+          </Typography>
+        </Grid>
+
+        {/* Regional Centre (Central) */}
+        <Grid item>
+          <Typography variant="body1">
+            <strong>Regional Centre (Central):</strong><br />
+            <h5>B-Wing, Kanikonna Villa, Infopark Campus<br />
+            Koratty, Thrissur, Kerala, India - 680 308</h5>
+          </Typography>
+        </Grid>
+      </Grid>
+
+      {/* Copyright Section */}
+      <Typography variant="body2" color="textSecondary" style={{ marginTop: '20px' }}>
+        &copy; {new Date().getFullYear()} ICTAK. All rights reserved.
+      </Typography>
+    </Box>
+  </Grid>
+</div>
+
+
   )
 }
 
